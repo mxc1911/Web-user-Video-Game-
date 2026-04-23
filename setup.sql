8a55b18ce3752e77edc46ae78fe199a2b2f2e09f
ConClr <conor2cleary@gmail.com>
2026-04-23 20:14:02 +0100

    Refactor SQL table creation for games and reviews

--- setup.sql
+++ setup.sql
@@ -35,6 +35,49 @@ create table public.reviews (
       (rating >= 1)
       and (rating <= 10)
     )
+
+    create table public.game_genres (
+  game_id integer not null,
+  genre_id integer not null,
+  constraint game_genres_pkey primary key (game_id, genre_id),
+  constraint game_genres_game_id_fkey foreign KEY (game_id) references games (id) on delete CASCADE,
+  constraint game_genres_genre_id_fkey foreign KEY (genre_id) references genres (id) on delete CASCADE
+) TABLESPACE pg_default;
   )
 ) TABLESPACE pg_default;
 
+create table public.game_tags (
+  game_id integer not null,
+  tag_id integer not null,
+  constraint game_tags_pkey primary key (game_id, tag_id),
+  constraint game_tags_game_id_fkey foreign KEY (game_id) references games (id) on delete CASCADE,
+  constraint game_tags_tag_id_fkey foreign KEY (tag_id) references tags (id) on delete CASCADE
+) TABLESPACE pg_default;
+
+create table public.platforms (
+  id serial not null,
+  name text not null,
+  description text null,
+  constraint platforms_pkey primary key (id)
+) TABLESPACE pg_default;
+
+create table public.genres (
+  id serial not null,
+  name text not null,
+  description text null,
+  constraint genres_pkey primary key (id)
+) TABLESPACE pg_default;
+
+create table public.game_platforms (
+  game_id integer not null,
+  platform_id integer not null,
+  constraint game_platforms_pkey primary key (game_id, platform_id),
+  constraint game_platforms_game_id_fkey foreign KEY (game_id) references games (id) on delete CASCADE,
+  constraint game_platforms_platform_id_fkey foreign KEY (platform_id) references platforms (id) on delete CASCADE
+) TABLESPACE pg_default;
+
+create table public.tags (
+  id serial not null,
+  name text not null,
+  constraint tags_pkey primary key (id)
+) TABLESPACE pg_default;
